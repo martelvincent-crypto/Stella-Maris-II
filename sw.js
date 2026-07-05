5b7c42ff3e9bb373bcf5bd923fc0dc5e703e485d
martelvincent-crypto <martel.vincent@gmail.com>
2026-07-05 15:31:50 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const V="stella-maris-v3";
+const FILES=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request)));});
